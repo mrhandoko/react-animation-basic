ac26c6dbcb4a3c7dc4689f30a3481a377c2ffdaf
mrhandoko <bambank.handoko@yahoo.com>
2017-07-17 17:56:49 +0700

    add lottie airbnb

--- App.js
+++ App.js
@@ -2,23 +2,12 @@ import React from 'react';
 import { StyleSheet, Animated, Easing, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  componentWillMount() {
-    this.animatedValue = new Animated.Value(100);
-  }
-  componentDidMount() {
-    Animated.timing(this.animatedValue, {
-      toValue: 150,
-      duration: 3000,
-      easing: Easing.bounce,
-    }).start();
-  }
   render() {
-    const animatedStyle = { height: this.animatedValue }
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Animated.View style={{ justifyContent:'center', alignItems: 'center', backgroundColor: '#000000', height: 100, width: 100, animatedStyle }}>
+        <View style={styles.box}>
           <Text style={{ color: "#FFFFFF" }}>Adams Hawa</Text>
-        </Animated.View>
+        </View>
       </View>
     )
   }
